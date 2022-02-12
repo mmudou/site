aca4e9842d49d870a1cbdb1fcb376ffdbb93fa15
Dylson Valente Neto <ama@n370.info>
2022-02-12 19:34:29 -0300

    setup prismic slicemachine

---
+++ .slicemachine/assets/slices/Bio/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Bio';
+
+export default {
+  title: 'slices/Bio'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"bio","items":[],"primary":{"title":[{"type":"heading1","text":"Deploy strategic models","spans":[]}],"description":[{"type":"paragraph","text":"Consequat commodo qui fugiat deserunt laborum voluptate amet elit pariatur cillum. Elit occaecat ut laboris sunt.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
